a4ee56e2432b44d8857b5d1c2008479e92807f7b
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-04 19:25:10 +0600

    Create Post

--- crud.ts
+++ crud.ts
@@ -1,13 +1,26 @@
 import { prisma } from "./lib/prisma";
 
 async function run() {
-    const createUser = await prisma.user.create({
+    // const createUser = await prisma.user.create({
+    //     data: {
+    //         name: "Jioun Rahman",
+    //         email: "jiounrahman@gmail.com",
+
+    //     }
+    // })
+    // // console.log("Create User", createUser);
+
+    // post 
+    const createPost = await prisma.post.create({
         data: {
-            name: "Jioun Rahman",
-            email: "jiounrahman@gmail.com",
+
+            title: "This title ",
+            content: "This Content",
+            isPublished: true,
+            authorId: 1
 
         }
     })
-    console.log("Create User", createUser);
+    console.log("Create Post" , createPost);
 }
 run();
